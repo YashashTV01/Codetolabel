5e04a1157e9524b1414da45ce5184873bcd9c931
YashashTV01 <140975563+YashashTV01@users.noreply.github.com>
2024-02-29 16:04:16 +0000

    Update labeling.py
    now we can upload dataset 
    can select column 
    we can download the labelled dataset

--- labeling.py
+++ labeling.py
@@ -6,18 +6,15 @@ from nltk.stem import WordNetLemmatizer
 import string
 from multiprocessing import Pool, cpu_count
 
-# Initialize NLTK resources
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 analyzer = SentimentIntensityAnalyzer()
 
-# Function for text preprocessing
 def preprocess_text(text):
     tokens = word_tokenize(text.lower())
     tokens = [lemmatizer.lemmatize(token) for token in tokens if token not in stop_words and token not in string.punctuation]
     return ' '.join(tokens)
 
-# Function to perform sentiment analysis
 def perform_sentiment_analysis(text):
     preprocessed_text = preprocess_text(text)
     compound_score = analyzer.polarity_scores(preprocessed_text)['compound']
@@ -32,7 +29,6 @@ def perform_sentiment_analysis(text):
     else:
         return "VeryNegative", compound_score
 
-# Load dataset
 def load_dataset(file_path):
     try:
         df = pd.read_csv(file_path)
@@ -41,32 +37,24 @@ def load_dataset(file_path):
         print("File not found.")
         return None
 
-# Main function
 def main():
-    # Input dataset path
     file_path = input("Enter the path to your dataset: ")
     df = load_dataset(file_path)
     if df is None:
         return
     
-    # Select column containing text data
     text_column = input("Enter the name of the column containing text data: ")
     
-    # Define the number of processes to use (adjust according to your CPU cores)
-    num_processes = cpu_count()  # Use all available CPU cores
+    num_processes = cpu_count()
     
-    # Perform sentiment analysis in parallel
     with Pool(processes=num_processes) as pool:
         results = pool.map(perform_sentiment_analysis, df[text_column])
     
-    # Extract sentiment labels and compound scores from the results
     sentiment_labels, compound_scores = zip(*results)
     
-    # Add sentiment labels and compound scores to the dataset
     df['SentimentLabel'] = sentiment_labels
     df['CompoundScore'] = compound_scores
     
-    # Save the updated dataset
     output_path = input("Enter the path to save the updated dataset: ")
     df.to_csv(output_path, index=False)
     print("Sentiment analysis completed and dataset updated successfully.")
